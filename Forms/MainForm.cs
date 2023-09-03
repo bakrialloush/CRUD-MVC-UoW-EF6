bf0cdbfd23760ef8cc889bd7f282b6787056d81b
Bakri <backarnet@gmail.com>
2023-09-03 12:58:40 +0300

    Handle Exceptions in forms

--- Forms/MainForm.cs
+++ Forms/MainForm.cs
@@ -10,7 +10,7 @@ namespace CRUD_UoW.Forms
         DBContext context;
         AuthorsController authors;
         PostsController posts;
-        int id = -1;
+        int id;
 
         public MainForm()
         {
@@ -23,109 +23,163 @@ namespace CRUD_UoW.Forms
 
         private void LoadGrids()
         {
+            id = 0;
             gridAuth.DataSource = authors.GetList();
             gridPost.DataSource = posts.GetList();
             foreach (Control control in Controls)
             {
                 control.Enabled = true;
             }
-            btnEditAuth.Enabled = false;
-            btnEditPost.Enabled = false;
-            textAuthName.Text = "";
-            textPostTitle.Text = "";
-            id = -1;
+            btnEditAuth.Enabled = btnEditPost.Enabled = false;
+            textAuthName.Text = textPostTitle.Text = "";
         }
 
         private void btnAddAuth_Click(object sender, EventArgs e)
         {
-            authors.Add(new Author
+            try
             {
-                Name = textAuthName.Text,
-            });
-            LoadGrids();
+                authors.Add(new Author
+                {
+                    Name = textAuthName.Text,
+                });
+                LoadGrids();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+            }
         }
 
         private void btnAddPost_Click(object sender, EventArgs e)
         {
-            int id = (int)gridAuth.SelectedRows[0].Cells["Id"].Value;
-            var auth = authors.GetSingle(id);
-            posts.Add(new Post
+            try
             {
-                Title = textPostTitle.Text,
-                Auth = auth,
-            });
-            LoadGrids();
+                int id = (int)gridAuth.SelectedRows[0].Cells["Id"].Value;
+                var auth = authors.GetSingle(id);
+                posts.Add(new Post
+                {
+                    Title = textPostTitle.Text,
+                    Auth = auth,
+                });
+                LoadGrids();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+            }
         }
 
         private void btnDelAuth_Click(object sender, EventArgs e)
         {
-            int id = (int)gridAuth.SelectedRows[0].Cells["Id"].Value;
-            authors.Delete(id);
-            LoadGrids();
+            try
+            {
+                int id = (int)gridAuth.SelectedRows[0].Cells["Id"].Value;
+                authors.Delete(id);
+                LoadGrids();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+            }
         }
 
         private void btnDelPost_Click(object sender, EventArgs e)
         {
-            int id = (int)gridPost.SelectedRows[0].Cells["Id"].Value;
-            posts.Delete(id);
-            LoadGrids();
+            try
+            {
+                int id = (int)gridPost.SelectedRows[0].Cells["Id"].Value;
+                posts.Delete(id);
+                LoadGrids();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+            }
         }
 
         private void gridAuth_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            var auth = authors.GetSingle((int)gridAuth["Id", e.RowIndex].Value);
-            if (auth != null)
+            try
             {
-                id = auth.Id;
-                textAuthName.Text = auth.Name;
+                var auth = authors.GetSingle((int)gridAuth["Id", e.RowIndex].Value);
+                if (auth != null)
+                {
+                    id = auth.Id;
+                    textAuthName.Text = auth.Name;
+                }
+                foreach (Control control in Controls)
+                {
+                    control.Enabled = false;
+                }
+                btnEditAuth.Enabled = true;
+                textAuthName.Enabled = true;
+                textAuthName.SelectAll();
+                textAuthName.Focus();
             }
-            foreach (Control control in Controls)
+            catch (Exception ex)
             {
-                control.Enabled = false;
+                MessageBox.Show(ex.Message, "Error", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
             }
-            btnEditAuth.Enabled = true;
-            textAuthName.Enabled = true;
-            textAuthName.SelectAll();
-            textAuthName.Focus();
         }
 
         private void gridPost_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
         {
-            var post = posts.GetSingle((int)gridPost["Id", e.RowIndex].Value);
-            if (post != null)
+            try
             {
-                id = post.Id;
-                textPostTitle.Text = post.Title;
+                var post = posts.GetSingle((int)gridPost["Id", e.RowIndex].Value);
+                if (post != null)
+                {
+                    id = post.Id;
+                    textPostTitle.Text = post.Title;
+                }
+                foreach (Control control in Controls)
+                {
+                    control.Enabled = false;
+                }
+                gridAuth.Enabled = true;
+                btnEditPost.Enabled = true;
+                textPostTitle.Enabled = true;
+                textPostTitle.SelectAll();
+                textPostTitle.Focus();
+                textAuthName.Focus();
             }
-            foreach (Control control in Controls)
+            catch (Exception ex)
             {
-                control.Enabled = false;
+                MessageBox.Show(ex.Message, "Error", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
             }
-            gridAuth.Enabled = true;
-            btnEditPost.Enabled = true;
-            textPostTitle.Enabled = true;
-            textPostTitle.SelectAll();
-            textPostTitle.Focus();
-            textAuthName.Focus();
         }
 
         private void btnEditAuth_Click(object sender, EventArgs e)
         {
-            var auth = authors.GetSingle(id);
-            auth.Name = textAuthName.Text;
-            authors.Update(auth);
-            LoadGrids();
+            try
+            {
+                var auth = authors.GetSingle(id);
+                auth.Name = textAuthName.Text;
+                authors.Update(auth);
+                LoadGrids();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+            }
         }
 
         private void btnEditPost_Click(object sender, EventArgs e)
         {
-            var auth = authors.GetSingle((int)gridAuth.SelectedRows[0].Cells["Id"].Value);
-            var post = posts.GetSingle(id);
-            post.Title = textPostTitle.Text;
-            post.Auth = auth;
-            posts.Update(post);
-            LoadGrids();
-            textPostTitle.Focus();
+            try
+            {
+                var auth = authors.GetSingle((int)gridAuth.SelectedRows[0].Cells["Id"].Value);
+                var post = posts.GetSingle(id);
+                post.Title = textPostTitle.Text;
+                post.Auth = auth;
+                posts.Update(post);
+                LoadGrids();
+                textPostTitle.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", buttons: MessageBoxButtons.OK, icon: MessageBoxIcon.Error);
+            }
         }
     }
 }
